b80468fd94d811a01792dded944216efd5ffa845
Stanislav Pelak <stanislav.pelak@ibm.com>
2023-09-21 11:30:16 +0200

    feat: travis deployment Refs: #5

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -ex
+
+curl https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.4/linux/oc.tar.gz | tar -xzvf -
+
+./oc login -u apikey -p $IBMCLOUD_API_KEY --server $OPENSHIFT_SERVER -n $OPENSHIFT_NAMESPACE
+./oc tag ${REGISTRY_URL}/${IMAGE_NAME}:${TAG_PREFIX}-${TRAVIS_COMMIT} ${APP_NAME}:latest
