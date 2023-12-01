c2f68ddbd82feb3adc298a574bd0c17196c62763
Stanislav Pelak <stanislav.pelak@ibm.com>
2023-12-02 00:31:18 +0100

    feat: landing page - map

--- src/page-content/landing/members.tsx
+++ src/page-content/landing/members.tsx
@@ -3,52 +3,54 @@ import { Column, Grid, Heading, Section } from '@carbon/react';
 import { motion } from 'framer-motion';
 import { showInView } from '@utils/showInView';
 import classnames from 'classnames';
+import MembersMapWithSidebar from '@graphics/membersMap.svg';
+import MembersMapOnly from '@graphics/membersMapOnly.svg';
 
 import styles from './members.module.scss';
 
-const MEMBERS = [
-  'A*STAR',
-  'Aitomatic',
-  'AMD',
-  'Anyscale',
-  'Cerebras',
-  'CERN',
-  'Cleveland Clinic',
-  'Dell Technologies',
-  'Ecole Polytechnique Federale de Lausanne (EPFL)',
-  'ETH Zurich',
-  'Fast.ai',
-  'FPT',
-  'Hebrew University',
-  'Hugging Face',
-  'IBM',
-  'Imperial College London',
-  'Indian Institute of Technology Bombay',
-  'International Center for Theoretical Physics',
-  'Institute for Computer Science, Artificial Intelligence',
-  'Intel',
-  'Keio University',
-  'LangChain',
-  'LlamaIndex',
-  'Mass Open Cloud Alliance',
-  'Meta',
-  'Mohamed bin Zayed University of Artificial Intelligence',
-  'MLCommons',
-  'National Aeronautics and Space Administration',
-  'National Science Foundation',
-  'NumFOCUS',
-  'Oracle',
-  'Quansight',
-  'Red Hat',
-  'Rensselaer Polytechnic Institute',
-  'Roadzen',
-  'Sakana AI',
-  'ServiceNow',
-  'Sony Group',
-  'Stability AI',
-  'Together AI',
-  'TU Munich',
-];
+// const MEMBERS = [
+//   'A*STAR',
+//   'Aitomatic',
+//   'AMD',
+//   'Anyscale',
+//   'Cerebras',
+//   'CERN',
+//   'Cleveland Clinic',
+//   'Dell Technologies',
+//   'Ecole Polytechnique Federale de Lausanne (EPFL)',
+//   'ETH Zurich',
+//   'Fast.ai',
+//   'FPT',
+//   'Hebrew University',
+//   'Hugging Face',
+//   'IBM',
+//   'Imperial College London',
+//   'Indian Institute of Technology Bombay',
+//   'International Center for Theoretical Physics',
+//   'Institute for Computer Science, Artificial Intelligence',
+//   'Intel',
+//   'Keio University',
+//   'LangChain',
+//   'LlamaIndex',
+//   'Mass Open Cloud Alliance',
+//   'Meta',
+//   'Mohamed bin Zayed University of Artificial Intelligence',
+//   'MLCommons',
+//   'National Aeronautics and Space Administration',
+//   'National Science Foundation',
+//   'NumFOCUS',
+//   'Oracle',
+//   'Quansight',
+//   'Red Hat',
+//   'Rensselaer Polytechnic Institute',
+//   'Roadzen',
+//   'Sakana AI',
+//   'ServiceNow',
+//   'Sony Group',
+//   'Stability AI',
+//   'Together AI',
+//   'TU Munich',
+// ];
 
 type MembersProps = {
   className?: string;
@@ -62,19 +64,37 @@ const Members: FC<MembersProps> = ({ className, sectionRef }) => (
     ref={sectionRef}
   >
     <Grid className={styles.members__grid}>
-      <Column xlg={{ span: 7, offset: 1 }} lg={7} md={6} sm={3}>
+      <Column xlg={{ span: 8, offset: 1 }} lg={7} md={6} sm={3}>
         <motion.div {...showInView}>
           <Heading>AI Alliance members</Heading>
         </motion.div>
       </Column>
       <Column xlg={{ span: 15, offset: 1 }} lg={16} md={8} sm={4}>
-        <ul className={styles.members__list}>
+        {/* <ul className={styles.members__list}>
           {MEMBERS.map((member, idx) => (
             <motion.li key={member} {...showInView} custom={idx * 0.1}>
-              {member}
+            {member}
             </motion.li>
-          ))}
-        </ul>
+            ))}
+          </ul> */}
+        <MembersMapWithSidebar
+          className={classnames(styles.map, styles.mapWithSidebar)}
+        />
+        <MembersMapOnly className={classnames(styles.map, styles.mapOnly)} />
+      </Column>
+      <Column xlg={0} lg={0} md={4} sm={4} className={styles.stat}>
+        <div className={styles.statTitle}>Total represented R&D funding</div>
+        <div className={styles.statValue}>&gt;$80B</div>
+      </Column>
+      <Column xlg={0} lg={0} md={4} sm={4} className={styles.stat}>
+        <div className={styles.statTitle}>
+          Students supported by these academic institutions
+        </div>
+        <div className={styles.statValue}>&gt;400,000</div>
+      </Column>
+      <Column xlg={0} lg={0} md={4} sm={4} className={styles.stat}>
+        <div className={styles.statTitle}>Total staff members</div>
+        <div className={styles.statValue}>&gt;100,000</div>
       </Column>
     </Grid>
   </Section>
