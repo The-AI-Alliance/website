92ef0fa8124fc4b7132387b44a2152fc424efaf3
Connor Leech <connor@ibm.com>
2023-12-04 00:27:58 -0500

    updating copy per latest

--- src/page-content/focusAreas/focusAreas.tsx
+++ src/page-content/focusAreas/focusAreas.tsx
@@ -15,40 +15,28 @@ type FocusAreaDetails = {
 
 const focusAreas: FocusAreaDetails[] = [
   {
+    title: 'Responsible, Scalable AI',
+    subtitle:
+      'Create benchmarks, tools, and methodologies to ensure and evaluate high-quality, safe, secure and trusted AI.',
+    body: 'AI Alliance members will deploy benchmarks, tools, and other resources that enable the responsible development and use of AI systems at global scale, including the creation of a catalog of vetted safety, security and trust tools. This work will include supporting the advocacy and enablement of these tools with the developer community for model and application development. AI Alliance members will also develop and align on meaningful benchmarks and evaluation standards for open model releases and model deployment into applications.',
+  },
+  {
+    title: 'Open Foundation Models',
+    subtitle:
+      'Enable an ecosystem of open foundation models with diverse modalities, including highly capable multilingual, multi-modal, and science models.',
+    body: 'The AI Alliance will focus on responsibly advancing the ecosystem of open foundation models with diverse modalities, including highly capable multilingual, multi-modal, and science models that can help address society-wide challenges in climate, human health, and beyond. To support AI model builders and application developers, AI Alliance members will join forces to help build and promote open-source tools for model training, tuning, and inference. The AI Alliance will also launch initiatives that encourage open development of AI in safe and beneficial ways, and host events to explore AI use cases.',
+  },
+  {
+    title: 'Diversified AI Hardware',
+    subtitle:
+      'Foster a vibrant AI hardware accelerator ecosystem by accelerating contributions and adoption of essential software technology.',
+    body: 'AI Alliance members will collaborate on the benchmarking, optimization, and adaptation of AI workloads to advance a diverse set of hardware. This work will focus on scalability, platform adaptation, and advanced power, energy, and carbon modeling. Benchmarks and metrics developed for model inference, fine-tuning and energy consumption of large-scale AI workloads will be contributed to the open-source community.',
+  },
+  {
     title: 'Skills, Education and Research',
     subtitle:
-      'Supporting global AI skills building, education, and exploratory research',
-    body: 'Engineers and scientists from AI Alliance member organizations will come together to collaborate on exploring and advancing a diverse set of research topics including AI algorithms, models, platforms, and new techniques for power and resource efficient AI. Members will also collaborate to help close the AI skills gap by training underrepresented communities and adult learners in generative AI.',
-  },
-  {
-    title: 'Safety and trust',
-    subtitle:
-      'Create testing, validation and benchmarking tools and methodologies to help build safe, secure, trusted and ethical AI',
-    body: 'Engineers and scientists from AI Alliance member organizations will come together to collaborate on exploring and advancing a diverse set of research topics including AI algorithms, models, platforms, and new techniques for power and resource efficient AI. Members will also collaborate to help close the AI skills gap by training underrepresented communities and adult learners in generative AI.',
-  },
-  {
-    title: 'Developer tools',
-    subtitle:
-      'Build and advance the most efficient and capable software frameworks and tools to enable AI model builders and GenAI application developers',
-    body: 'Engineers and scientists from AI Alliance member organizations will come together to collaborate on exploring and advancing a diverse set of research topics including AI algorithms, models, platforms, and new techniques for power and resource efficient AI. Members will also collaborate to help close the AI skills gap by training underrepresented communities and adult learners in generative AI.',
-  },
-  {
-    title: 'AI Hardware',
-    subtitle:
-      'Foster a vibrant AI hardware accelerator ecosystem by boosting contributions and adoption of essential enabling software technology',
-    body: 'Engineers and scientists from AI Alliance member organizations will come together to collaborate on exploring and advancing a diverse set of research topics including AI algorithms, models, platforms, and new techniques for power and resource efficient AI. Members will also collaborate to help close the AI skills gap by training underrepresented communities and adult learners in generative AI.',
-  },
-  {
-    title: 'Open Foundation models',
-    subtitle:
-      'Enable an ecosystem of open foundation models with diverse modalities, including highly capable multilingual, multi-modal, and science models',
-    body: 'Engineers and scientists from AI Alliance member organizations will come together to collaborate on exploring and advancing a diverse set of research topics including AI algorithms, models, platforms, and new techniques for power and resource efficient AI. Members will also collaborate to help close the AI skills gap by training underrepresented communities and adult learners in generative AI.',
-  },
-  {
-    title: 'Advocacy',
-    subtitle:
-      'Advocate for regulatory policies that create a healthy open ecosystem for AI',
-    body: 'Engineers and scientists from AI Alliance member organizations will come together to collaborate on exploring and advancing a diverse set of research topics including AI algorithms, models, platforms, and new techniques for power and resource efficient AI. Members will also collaborate to help close the AI skills gap by training underrepresented communities and adult learners in generative AI.',
+      'Support global AI skills building, education, and exploratory research.',
+    body: 'To help close the AI skill gap, AI Alliance members will engage the academic community to enable researchers and students to learn and contribute to essential AI model and tool research projects including AI algorithms, models, platforms, and new techniques for power and resource efficient AI.  Members will also collaborate to develop educational content and resources that inform the public discourse and policymakers on benefits, risks, solutions, and precision regulation for AI.',
   },
 ];
 
@@ -69,12 +57,10 @@ const FocusAreasPage: React.FC = () => {
             sm={4}
           >
             <motion.p className={styles.paragraph} {...showInView}>
-              The AI Alliance is focused on accelerating and disseminating open
-              innovation across the AI technology landscape to improve
-              foundational capabilities, ensure safety, security and trust in
-              AI, and responsibly maximize benefits to people and society
-              everywhere. Alliance members intend to start or enhance projects
-              across six focus areas.
+              AI Alliance members intend to start or enhance projects aligned
+              with its mission to accelerate open innovation across the AI
+              technology landscape that responsibly benefits people and society
+              everywhere.
             </motion.p>
           </Column>
         </Grid>
