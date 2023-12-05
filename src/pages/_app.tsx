ffde93c69b6662f9d9c9613e2addcf23256bfdbe
Stanislav Pelak <stanislav.pelak@ibm.com>
2023-12-05 06:04:10 +0100

    google analytics script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
 import type { AppProps } from 'next/app';
 import { NotificationProvider } from '@components/notificationwrapper/index.context';
 import '@styles/globals.scss';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NotificationProvider>
       <Component {...pageProps} />
+
+      <div className="ga_container">
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4D79BD68C0"
+        />
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              
+              gtag('config', 'G-4D79BD68C0');
+            `}
+        </Script>
+      </div>
     </NotificationProvider>
   );
 }
